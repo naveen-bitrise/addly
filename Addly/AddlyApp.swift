5cbc10c2f31a473a37e3daf654017f4956abf9f2
Ben Boral <ben.boral@bitrise.io>
2021-07-26 14:29:30 -0500

    Initial Commit

---
+++ Addly/AddlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddlyApp.swift
+//  Addly
+//
+//  Created by Ben Boral on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AddlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
