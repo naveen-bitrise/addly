e25b6df85c6d7a137b4f321fc27aec16ecf40120
Ben Boral <ben.boral@bitrise.io>
2021-07-27 19:32:49 -0500

    lints the existing files

--- Addly/NumbersOnly.swift
+++ Addly/NumbersOnly.swift
@@ -11,7 +11,7 @@ class NumbersOnly: ObservableObject {
     @Published var value = "" {
         didSet {
             let filtered = value.filter { $0.isNumber }
-            
+
             if value != filtered {
                 value = filtered
             }
