2d3b29f3715975cc72a98135051f215144b74a9f
Ben Boral <ben.boral@bitrise.io>
2024-02-16 17:25:06 -0600

    adds flaky tests

---
+++ AddlyTests/FlakyAdderTests.swift
@@ -0,0 +1,521 @@
+//
+//  FlakyTests.swift
+//  AddlyTests
+//
+//  Created by Ben Boral on 2/16/24.
+//
+
+import XCTest
+
+final class FlakyAdderTests: XCTestCase {
+
+    func testRand100Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 100)
+    }
+
+
+    func testRand101Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 101)
+    }
+
+
+    func testRand102Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 102)
+    }
+
+
+    func testRand103Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 103)
+    }
+
+
+    func testRand104Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 104)
+    }
+
+
+    func testRand105Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 105)
+    }
+
+
+    func testRand106Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 106)
+    }
+
+
+    func testRand107Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 107)
+    }
+
+
+    func testRand108Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 108)
+    }
+
+
+    func testRand109Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 109)
+    }
+
+
+    func testRand110Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 110)
+    }
+
+
+    func testRand111Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 111)
+    }
+
+
+    func testRand112Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 112)
+    }
+
+
+    func testRand113Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 113)
+    }
+
+
+    func testRand114Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 114)
+    }
+
+
+    func testRand115Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 115)
+    }
+
+
+    func testRand116Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 116)
+    }
+
+
+    func testRand117Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 117)
+    }
+
+
+    func testRand118Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 118)
+    }
+
+
+    func testRand119Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 119)
+    }
+
+
+    func testRand120Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 120)
+    }
+
+
+    func testRand121Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 121)
+    }
+
+
+    func testRand122Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 122)
+    }
+
+
+    func testRand123Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 123)
+    }
+
+
+    func testRand124Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 124)
+    }
+
+
+    func testRand125Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 125)
+    }
+
+
+    func testRand126Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 126)
+    }
+
+
+    func testRand127Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 127)
+    }
+
+
+    func testRand128Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 128)
+    }
+
+
+    func testRand129Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 129)
+    }
+
+
+    func testRand130Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 130)
+    }
+
+
+    func testRand131Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 131)
+    }
+
+
+    func testRand132Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 132)
+    }
+
+
+    func testRand133Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 133)
+    }
+
+
+    func testRand134Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 134)
+    }
+
+
+    func testRand135Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 135)
+    }
+
+
+    func testRand136Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 136)
+    }
+
+
+    func testRand137Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 137)
+    }
+
+
+    func testRand138Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 138)
+    }
+
+
+    func testRand139Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 139)
+    }
+
+
+    func testRand140Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 140)
+    }
+
+
+    func testRand141Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 141)
+    }
+
+
+    func testRand142Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 142)
+    }
+
+
+    func testRand143Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 143)
+    }
+
+
+    func testRand144Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 144)
+    }
+
+
+    func testRand145Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 145)
+    }
+
+
+    func testRand146Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 146)
+    }
+
+
+    func testRand147Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 147)
+    }
+
+
+    func testRand148Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 148)
+    }
+
+
+    func testRand149Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 149)
+    }
+
+
+    func testRand150Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 150)
+    }
+
+
+    func testRand151Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 151)
+    }
+
+
+    func testRand152Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 152)
+    }
+
+
+    func testRand153Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 153)
+    }
+
+
+    func testRand154Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 154)
+    }
+
+
+    func testRand155Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 155)
+    }
+
+
+    func testRand156Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 156)
+    }
+
+
+    func testRand157Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 157)
+    }
+
+
+    func testRand158Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 158)
+    }
+
+
+    func testRand159Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 159)
+    }
+
+
+    func testRand160Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 160)
+    }
+
+
+    func testRand161Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 161)
+    }
+
+
+    func testRand162Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 162)
+    }
+
+
+    func testRand163Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 163)
+    }
+
+
+    func testRand164Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 164)
+    }
+
+
+    func testRand165Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 165)
+    }
+
+
+    func testRand166Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 166)
+    }
+
+
+    func testRand167Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 167)
+    }
+
+
+    func testRand168Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 168)
+    }
+
+
+    func testRand169Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 169)
+    }
+
+
+    func testRand170Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 170)
+    }
+
+
+    func testRand171Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 171)
+    }
+
+
+    func testRand172Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 172)
+    }
+
+
+    func testRand173Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 173)
+    }
+
+
+    func testRand174Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 174)
+    }
+
+
+    func testRand175Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 175)
+    }
+
+
+    func testRand176Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 176)
+    }
+
+
+    func testRand177Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 177)
+    }
+
+
+    func testRand178Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 178)
+    }
+
+
+    func testRand179Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 179)
+    }
+
+
+    func testRand180Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 180)
+    }
+
+
+    func testRand181Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 181)
+    }
+
+
+    func testRand182Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 182)
+    }
+
+
+    func testRand183Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 183)
+    }
+
+
+    func testRand184Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 184)
+    }
+
+
+    func testRand185Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 185)
+    }
+
+
+    func testRand186Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 186)
+    }
+
+
+    func testRand187Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 187)
+    }
+
+
+    func testRand188Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 188)
+    }
+
+
+    func testRand189Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 189)
+    }
+
+
+    func testRand190Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 190)
+    }
+
+
+    func testRand191Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 191)
+    }
+
+
+    func testRand192Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 192)
+    }
+
+
+    func testRand193Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 193)
+    }
+
+
+    func testRand194Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 194)
+    }
+
+
+    func testRand195Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 195)
+    }
+
+
+    func testRand196Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 196)
+    }
+
+
+    func testRand197Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 197)
+    }
+
+
+    func testRand198Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 198)
+    }
+
+
+    func testRand199Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 199)
+    }
+
+
+    func testRand200Under2() {
+        generateAndCheckRandomNumber(fromOneTo: 200)
+    }
+    
+    func generateAndCheckRandomNumber(fromOneTo upperBound: Int) {
+        let randomNumber = Int.random(in: 1...upperBound)
+        XCTAssert(randomNumber > 5, "Random number \(randomNumber) is not above 5")
+    }
+
+}
